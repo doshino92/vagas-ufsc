2292afe76bf074ed0a18e1cee4f110566f8fa835
Guilherme dos Santos Hino <guilherme_sh@hotmail.com>
2026-06-25 22:33:29 -0300

    implementação busca de vaga por id

--- backend/src/routes/jobRoutes.js
+++ backend/src/routes/jobRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
     createJobController,
     getAllJobsController,
+    getJobByIdController,
 } = require("../controllers/jobController");
 
 const router = express.Router();
 
 router.post("/", createJobController);
 router.get("/", getAllJobsController);
+router.get("/:id", getJobByIdController);
 
 module.exports = router;
