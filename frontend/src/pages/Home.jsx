7e6b4b8cdd1f9e526c1b3d9992d0abd4108b6070
Guilherme dos Santos Hino <guilherme_sh@hotmail.com>
2026-06-27 02:50:26 -0300

    implementa estrutura inicial do frontend em React

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import SearchPanel from "../components/SearchPanel";
+
+function Home() {
+    return (
+        <>
+            <h2>Página Inicial</h2>
+            <SearchPanel />
+        </>
+    );
+}
+
+export default Home;
