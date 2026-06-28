ee1c3f215a29b9d47059d6b3ecbd2060c75e72fa
Guilherme dos Santos Hino <guilherme_sh@hotmail.com>
2026-06-28 01:13:02 -0300

    feat: implementa cadastro de vagas pelo frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import SearchPanel from "../components/SearchPanel";
 import JobFeed from "../components/JobFeed";
+import JobForm from "../components/JobForm";
 
 function Home() {
-
+    console.log("Renderizando Home");
     const [search, setSearch] = useState("");
+    const [reloadJobs, setReloadJobs] = useState(false);
+
+    function handleJobCreated() {
+        setReloadJobs((previousState) => !previousState);
+    }
 
     return (
-        <>
-            <h2>Página Inicial</h2>
+        <main>
+            <h1>Portal de Vagas UFSC</h1>
+
+            <JobForm
+                onJobCreated={handleJobCreated}
+            />
 
             <SearchPanel
                 search={search}
@@ -17,8 +27,9 @@ function Home() {
 
             <JobFeed
                 search={search}
+                reloadJobs={reloadJobs}
             />
-        </>
+        </main>
     );
 }
 
--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -10,4 +10,64 @@ async function getJobs() {
     return await response.json();
 }
 
-export { getJobs };
+async function getJobById(id) {
+    const response = await fetch(`${API_URL}/${id}`);
+
+    if (!response.ok) {
+        throw new Error("Vaga não encontrada.");
+    }
+
+    return await response.json();
+}
+
+async function createJob(job) {
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(job),
+    });
+
+    if (!response.ok) {
+        throw new Error("Erro ao criar vaga.");
+    }
+
+    return await response.json();
+}
+
+async function updateJob(id, job) {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(job),
+    });
+
+    if (!response.ok) {
+        throw new Error("Erro ao atualizar vaga.");
+    }
+
+    return await response.json();
+}
+
+async function deleteJob(id) {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+    });
+
+    if (!response.ok) {
+        throw new Error("Erro ao remover vaga.");
+    }
+
+    return await response.json();
+}
+
+export {
+    getJobs,
+    getJobById,
+    createJob,
+    updateJob,
+    deleteJob,
+};
