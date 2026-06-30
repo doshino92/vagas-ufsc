36ba6d96e9ca143f618b4e44e3857e5ffc5dfefe
Guilherme dos Santos Hino <guilherme_sh@hotmail.com>
2026-06-30 04:06:24 -0300

    Fix frontend nginx routing

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,31 +1,89 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import ProtectedRoute from "../components/ProtectedRoute";
+
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import EditAccount from "../pages/EditAccount";
 import JobDetails from "../pages/JobDetails";
 import EditJob from "../pages/EditJob";
+import MyApplications from "../pages/MyApplications";
+import JobApplications from "../pages/JobApplications";
+import Notifications from "../pages/Notifications";
 
 function AppRoutes() {
     return (
         <Routes>
+            {/* Públicas */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* Protegidas */}
             <Route
                 path="/"
-                element={<Home />}
+                element={
+                    <ProtectedRoute>
+                        <Home />
+                    </ProtectedRoute>
+                }
+            />
+
+            <Route
+                path="/account"
+                element={
+                    <ProtectedRoute>
+                        <EditAccount />
+                    </ProtectedRoute>
+                }
             />
 
             <Route
                 path="/jobs/:id"
-                element={<JobDetails />}
+                element={
+                    <ProtectedRoute>
+                        <JobDetails />
+                    </ProtectedRoute>
+                }
             />
 
             <Route
                 path="/jobs/:id/edit"
-                element={<EditJob />}
+                element={
+                    <ProtectedRoute>
+                        <EditJob />
+                    </ProtectedRoute>
+                }
             />
 
             <Route
-                path="*"
-                element={<Navigate to="/" replace />}
+                path="/my-applications"
+                element={
+                    <ProtectedRoute>
+                        <MyApplications />
+                    </ProtectedRoute>
+                }
             />
+
+            <Route
+                path="/jobs/:id/applications"
+                element={
+                    <ProtectedRoute>
+                        <JobApplications />
+                    </ProtectedRoute>
+                }
+            />
+
+            <Route
+                path="/notifications"
+                element={
+                    <ProtectedRoute>
+                        <Notifications />
+                    </ProtectedRoute>
+                }
+            />
+
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     );
 }
